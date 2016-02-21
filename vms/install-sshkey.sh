b796fc9b66e0c6e1150c5fd593e38a2ae5805c23
djvipmax <djvipmax@gmail.com>
2016-02-21 15:42:02 +0300

    test console commit

--- vms/install-sshkey.sh
+++ vms/install-sshkey.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
 !/bin/sh
 
 if [ ! -f ~/.ssh/id_rsa_vagrant.pub ]; then
-    cp /home/vagrant/project/vms/sshkey/id_rsa.pub /home/vagrant/.ssh/id_rsa_vagrant.pub;
+    cp /home/vagrant/project/vms/sshkey/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub;
+    cp /home/vagrant/project/vms/sshkey/id_rsa /home/vagrant/.ssh/id_rsa;
+    sudo chown vagrant /home/vagrant/.ssh/id_rsa
+    sudo chown vagrant /home/vagrant/.ssh/id_rsa.pub
     cat /home/vagrant/project/vms/sshkey/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
     echo 'The key from sshkey has been succesfully installed'
 else
